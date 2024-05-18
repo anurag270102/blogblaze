e34bd5ecac8121448bde328fe25887c06e12455d
anurag270102 <anuragdalsaniya@gmail.com>
2024-05-18 18:58:09 +0530

    all setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <img src="src/assets/Colorful Modern Concept Free B Logo.svg"  />
+    </h1>
+  )
+}
